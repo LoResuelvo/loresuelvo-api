28bd814889df37913aa662d86c8454d7af666033
Andres Colina <acolina@fi.uba.ar>
2026-05-03 22:40:04 -0300

    refactor: Extract logic of routing from main

--- main.go
+++ main.go
@@ -1,24 +1,13 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
+	"github.com/LoResuelvo/loresuelvo-api/pkg/router"
 )
 
-// setupRouter configura la ruta principal
-func setupRouter() *gin.Engine {
-	r := gin.Default()
-
-	// Ruta raíz que devuelve "Hola mundo" en texto plano
-	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hola mundo")
-	})
-
-	return r
-}
-
 func main() {
-	r := setupRouter()
-	r.Run(":8080")
+	r := router.SetupRouter()
+	err := r.Run(":8080")
+	if err != nil {
+		panic(err)
+	}
 }
