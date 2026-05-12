7cc58e166e6ff860989cb6e1a226a256a8b2124e
joseph_mt <jamamani@fi.uba.ar>
2026-05-12 23:08:46 +0000

    add[us-1.1]: base configuration and dependencies for postgres

---
+++ db/postgres.go
@@ -0,0 +1,48 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+const defaultPostgresURL = "postgres://postgres:postgres@localhost:5432/loresuelvo?sslmode=disable"
+
+type PostgresConfig struct {
+	URL string
+}
+
+func NewPostgresConfigFromEnv() PostgresConfig {
+	return PostgresConfig{
+		URL: envOrDefault("DATABASE_URL", defaultPostgresURL),
+	}
+}
+
+func ConnectPostgres(ctx context.Context, config PostgresConfig) (*sql.DB, error) {
+	database, err := sql.Open("pgx", config.URL)
+	if err != nil {
+		return nil, err
+	}
+
+	pingCtx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	if err := database.PingContext(pingCtx); err != nil {
+		_ = database.Close()
+		return nil, err
+	}
+
+	return database, nil
+}
+
+func envOrDefault(key string, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+
+	return value
+}
