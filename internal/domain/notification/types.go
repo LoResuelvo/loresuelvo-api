b7053e9c6088b664a79d8a59d364f80d64ba2b18
Andres Colina <acolina@fi.uba.ar>
2026-07-12 22:30:40 -0300

    feat[55.1]: added types for new type of notification

--- internal/domain/notification/types.go
+++ internal/domain/notification/types.go
@@ -3,13 +3,15 @@ package notification
 type Type string
 
 const (
-	TypeServiceProposalReceived Type = "service_proposal_received"
-	TypeServiceProposalAccepted Type = "service_proposal_accepted"
-	TypeServiceProposalRejected Type = "service_proposal_rejected"
+	TypeServiceProposalReceived       Type = "service_proposal_received"
+	TypeServiceProposalAccepted       Type = "service_proposal_accepted"
+	TypeServiceProposalRejected       Type = "service_proposal_rejected"
+	TypeWorkOrderCloseToScheduledTime Type = "work_order_close_to_scheduled_time"
 )
 
 type ResourceType string
 
 const (
 	ResourceServiceProposal ResourceType = "service_proposal"
+	ResourceWorkOrder       ResourceType = "work_order"
 )
