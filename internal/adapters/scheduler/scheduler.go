a8d287f2e3266898d8f56fc835ace50c56a65db0
Andres Colina <acolina@fi.uba.ar>
2026-07-12 21:41:39 -0300

    feat[55.1]: Scheduler adapter

---
+++ internal/adapters/scheduler/scheduler.go
@@ -0,0 +1,42 @@
+package scheduler
+
+import (
+	"context"
+	"time"
+)
+
+type Task interface {
+	Execute() error
+}
+
+type scheduler struct {
+	interval time.Duration
+	task     Task
+}
+
+func NewScheduler(interval time.Duration, task Task) *scheduler {
+	return &scheduler{
+		interval: interval,
+		task:     task,
+	}
+}
+
+func (s *scheduler) Run(ctx context.Context) {
+	ticker := time.NewTicker(s.interval)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ticker.C:
+			s.executeTask()
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+func (s *scheduler) executeTask() {
+	if err := s.task.Execute(); err != nil {
+		// TODO: LOGGER aqui.
+		return
+	}
+}
